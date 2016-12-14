3eb14a08f7ca28074bc7f02634a7aaa7f7e3ea76
Jeremy Mayeres <jeremy@planet.com>
2016-12-14 17:45:09 +0100

    Initial base files

---
+++ README.md
@@ -0,0 +1,3 @@
+# PL Gallery Grabber
+
+Grabs the latest gallery images, useful if you like having gallery images for desktop background
---
+++ setup.py
@@ -0,0 +1,53 @@
+from setuptools import (
+    setup,
+    find_packages,
+    os,
+)
+from io import open
+
+
+base_directory = os.path.dirname(__file__)
+
+
+def get_module_name(line):
+    filename = line.split('/')[-1]
+    module_name = filename.split('.tar.gz')[0]
+    return '-'.join(module_name.split('-')[:-1])
+
+
+def get_requirements(filename):
+    content = []
+    with open(os.path.join(base_directory, filename)) as f:
+        for line in f:
+            # Temporary computation needed for planet-common-client and modules installed from GitHub
+            if line.startswith('-e'):
+                module = line.split("#egg=")[1]
+            elif line.startswith(('https://', 'http://')):
+                module = get_module_name(line)
+            else:
+                module = line
+            content.append(module)
+        return content
+
+dev_requires = []
+install_requires = get_requirements('requirements.txt')
+#tests_require = get_requirements('requirements_for_test.txt')
+
+COVERAGE_PACKAGES = find_packages(base_directory, exclude=["core", "*_tests", "*.*_tests", "*.migrations"])
+
+if __name__ == "__main__":
+    setup(
+        name='pl-gallery-grabber',
+        version='0.1',
+        author='Jeremy Mayeres',
+        url='https://hello.planet.com/code/jeremy/pl-gallery-grabber',
+        description='Planet Gallery Image Grabber',
+        long_description=open('README.md', encoding='utf-8').read(),
+        packages=find_packages(exclude=('*tests', )),
+        install_requires=install_requires,
+        extras_require={
+ #           'tests': tests_require,
+            'dev': dev_requires,
+        },
+    )
+   
